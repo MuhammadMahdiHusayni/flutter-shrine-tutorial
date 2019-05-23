d2b3432f8d1de7433a3bda33d976b14c672c1075
Muhammad Mahdi Husayni bin Azizan <mmhusayni2@gmail.com>
2019-05-23 15:06:09 +0800

    Center and drop the text

--- lib/home.dart
+++ lib/home.dart
@@ -37,7 +37,7 @@ class HomePage extends StatelessWidget {
         // TODO: Adjust card heights (103)
         child: Column(
           // TODO: Center items on the card (103)
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             AspectRatio(
               aspectRatio: 18 / 11,
@@ -53,7 +53,9 @@ class HomePage extends StatelessWidget {
                 padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
                 child: Column(
                   // TODO: Align labels to the bottom and center (103)
-                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   // TODO: Change innermost Column (103)
                   children: <Widget>[
                     // TODO: Handle overflowing labels (103)
